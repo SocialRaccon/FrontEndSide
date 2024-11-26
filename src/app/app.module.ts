c14133072442e00d57b6793d7a2c11fd42c41286
Gera1UP <monterovallejogerardo@gmail.com>
2024-11-26 13:43:41 -0600

    Cambios en el header y footer del login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import { CoreModule } from './core/core.module';
 import { AuthModule } from './features/auth/auth.module';
 import { FeaturesModule } from './features/features.module';
 import {AuthInterceptor} from "./core/interceptors/auth.interceptor";
-
 import { LoginComponent } from './auth/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ToastrModule } from 'ngx-toastr';
@@ -19,7 +18,7 @@ import { ToastrModule } from 'ngx-toastr';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
